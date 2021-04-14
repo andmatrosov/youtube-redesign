c22a3e945f5c6bd89aa5d1d71967e66eee4967d0
andmatrosov <and.matrosow@ya.ru>
2021-04-15 00:36:35 +0300

    Добавил слайдер

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 'auto',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
