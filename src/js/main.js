e90f22e072e5d6afe2e3ea54a89529bc9f1897e0
andmatrosov <and.matrosow@ya.ru>
2021-04-15 23:48:32 +0300

    Добавил контент, настроил слайдеры

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,35 @@
-const swiper = new Swiper('.swiper-container', {
+const myChannelSlider = new Swiper('.my-channel-slider', {
   // Optional parameters
   loop: true,
   slidesPerView: 'auto',
 
   // Navigation arrows
   navigation: {
-    nextEl: '.channel-button-next',
-    prevEl: '.channel-button-prev',
+    nextEl: '#my-channel-button-next',
+    prevEl: '#my-channel-button-prev',
+  },
+});
+
+const recommendedSlider = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 'auto',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#recommended-button-next',
+    prevEl: '#recommended-button-prev',
+  },
+});
+
+const recommendedChannelSlider = new Swiper('.recommended-channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 'auto',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '#recommended-channel-button-next',
+    prevEl: '#recommended-channel-button-prev',
   },
 });
