c2a8283ba3b6a8844d521b1b7918f4c1c29217d2
andmatrosov <and.matrosow@ya.ru>
2021-04-19 23:23:22 +0300

    Добавлены стили для адаптивности

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,25 @@
 const myChannelSlider = new Swiper('.my-channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
 
   // Navigation arrows
   navigation: {
@@ -13,7 +31,16 @@ const myChannelSlider = new Swiper('.my-channel-slider', {
 const recommendedSlider = new Swiper('.recommended-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    1600: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 2,
+    },
+  },
 
   // Navigation arrows
   navigation: {
@@ -25,7 +52,25 @@ const recommendedSlider = new Swiper('.recommended-slider', {
 const recommendedChannelSlider = new Swiper('.recommended-channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 'auto',
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    1900: {
+      slidesPerView: 6,
+    },
+    1600: {
+      slidesPerView: 5,
+    },
+    1300: {
+      slidesPerView: 4,
+    },
+    1100: {
+      slidesPerView: 3,
+    },
+    800: {
+      slidesPerView: 2,
+    },
+  },
 
   // Navigation arrows
   navigation: {
@@ -33,3 +78,16 @@ const recommendedChannelSlider = new Swiper('.recommended-channel-slider', {
     prevEl: '#recommended-channel-button-prev',
   },
 });
+
+const searchBtn = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+
+searchBtn.addEventListener('click', () => {
+  mobileSearch.classList.toggle('isOpen');
+});
+
+if (document.documentElement.scrollWidth <= 640) {
+  myChannelSlider.destroy();
+  recommendedSlider.destroy();
+  recommendedChannelSlider.destroy();
+}
